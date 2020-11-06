7a5ad356895add6099a67cd3b2625fd839af68da
Pratyuh <pratyusacool@gmail.com>
2020-11-06 13:20:11 +0530

    create input and fetch result

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainPagelayout from '../components/MainPagelayout'
 
 const Home = () => {
+
+    const [input,setInput] = useState('')
+
+    const onSearch = () => {
+
+        // https://api.tvmaze.com/search/shows?q=girls
+
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).
+        then(r => r.json()).
+        then(result => {
+            console.log(result)
+        })
+
+    }
+
+    const onInputChange = (ev) =>{
+        setInput(ev.target.value)
+    }
+
+
+    const onKeyDown = (ev) => {
+        if (ev.keyCode === 13){
+            onSearch()
+        }
+    }
+
+ 
+
+
     return (
         <MainPagelayout>
-            Home Page
+           <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+           <button type="button" onClick={onSearch}>Search</button>
         </MainPagelayout>
     )
 }
