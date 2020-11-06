327e7b58bdf3da634dcd25c1cd31df4ab2183d66
Pratyuh <pratyusacool@gmail.com>
2020-11-06 11:30:53 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
+import { Switch,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-   <h1>Hii</h1>
-    </div>
+    <Switch>
+
+
+    <Route exact path="/">This is Home Page</Route>
+    <Route exact path="/contact">This is Contact Page</Route>
+    <Route>This is Invalid Page</Route>
+
+    </Switch>
   );
 }
 
